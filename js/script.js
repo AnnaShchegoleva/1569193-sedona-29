8349b6accc4723aac44e7f6305e317d14f81f344
AnnaShchegoleva <70628098+AnnaShchegoleva@users.noreply.github.com>
2020-11-10 10:41:01 -0800

    изменен js скрипт по результатам проверки

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const form = document.querySelector(".popup");
 const formOpenButton = document.querySelector(".hotel-search-button");
 
+form.classList.toggle("visually-hidden");
+
 formOpenButton.addEventListener("click", function () {
   form.classList.toggle("visually-hidden");
 });
